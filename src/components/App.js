866112149f7a385e20e6441537fece35924cae78
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-10-29 01:15:32 +0300

    small fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
+import { Link, NavLink, Route, Switch } from "react-router-dom";
 
 // Components used for the different routes
-import Home from './Home'
-import ItemsList from './ItemsList'
-import Item from './Item'
+import Home from "./Home";
+import ItemsList from "./ItemsList";
+import Item from "./Item";
 
 // Dummy data
-import data from '../data'
+import data from "../data";
 
-function fetchStock() {
-  // fetchStock simulates getting data through axios.get(<URL>)
-  return Promise.resolve({ success: true, data })
-}
-
-export default function App(props) {
-  const [stock, setStock] = useState([])
-
-  useEffect(() => {
-    fetchStock().then(res => setStock(res.data))
-  }, [])
+export default function App() {
+  const [stock, setStock] = useState([data]);
 
   return (
-    <div className='App'>
-      <nav>
-        <h1 className='store-header'>Emily&apos;s Trinkets</h1>
-        <div className='nav-links'>
+    <div className="App">
+      <nav style={{ listStyle: "none" }}>
+        <h1 className="store-header">Emily&apos;s Trinkets</h1>
+        <div className="nav-links">
           {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
+          <li>
+            <NavLink to="/" style={{ margin: "15px" }}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/items-list" style={{ margin: "15px" }}>
+              Shop
+            </NavLink>
+          </li>
         </div>
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
+      <Switch>
+        <Route exact path="/items-list">
+          <ItemsList items={stock} />
+        </Route>
+
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
-  )
+  );
 }
